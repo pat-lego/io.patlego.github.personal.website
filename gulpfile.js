e76523b58b8742205f5f0b12101f80512be26182
Patrique Legault <patrique.legault@gmail.com>
2024-12-13 09:36:29 -0500

    Adding css processing and build optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 import gulp from 'gulp';
 import { deleteSync } from 'del';
 import fileInclude from 'gulp-file-include';
+import sourcemaps from 'gulp-sourcemaps';
+import postcss from 'gulp-postcss';
 import browserSync from 'browser-sync';
 
 const browser = browserSync.create();
 
+// Paths
+const paths = {
+  html: {
+    src: 'src/**/*.html',
+    dest: 'dist/',
+  },
+  css: {
+    src: 'src/styles/tailwind.css',
+    dest: 'dist/css',
+  },
+};
+
 // Task to clean the dist folder
-export const clean = (done) => {
+export const dlte = (done) => {
   deleteSync('dist/**'); // Deletes everything in dist except the folder itself
   done();
 };
@@ -25,19 +39,34 @@ export const html = () => {
     .pipe(browser.stream());
 };
 
+// Task to process CSS using PostCSS
+export const css = () => {
+  return gulp
+    .src(paths.css.src)
+    .pipe(sourcemaps.init())
+    .pipe(postcss())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(browser.stream());
+};
+
 // Task to serve files with live reload
-export const serve = () => {
+export const watch = () => {
   browser.init({
     server: {
       baseDir: 'dist',
     },
   });
 
-  gulp.watch('src/**/*.html', html);
+  gulp.watch(paths.html.src, html);
+  gulp.watch('src/styles/**/*.css', css);
 };
 
 // Default task executed by running yarn gulp
-export default gulp.series(clean, html, serve);
+export default gulp.series(dlte, html, css, watch);
 
 // can be executed by yarn gulp build
-export const build = gulp.series(clean, html);
+export const build = gulp.series(dlte, css, html);
+
+// can be executed by yarn gulp build
+export const clean = gulp.series(dlte);
